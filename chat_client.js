200db2737ce347460b8a99cba285265c3acecdfb
Midlaj C <midlajc@gmail.com>
2021-07-15 06:57:32 +0530

    cors added

--- chat_client.js
+++ chat_client.js
@@ -1,9 +1,9 @@
-var socket = require('socket.io-client')('https://teli-chat.herokuapp.com:3000');
+var socket = require('socket.io-client')('http://localhost:3000');
 const repl = require('repl')
 const chalk = require('chalk');
 let username = null
 
-socket.on('disconnect',()=>{
+socket.on('disconnect', () => {
     socket.emit('disconnect')
 })
 
--- chat_server.js
+++ chat_server.js
@@ -1,6 +1,12 @@
 const http = require('http').createServer();
 const io = require('socket.io')(http);
+var express = require('express')
+let exp = express()
+var cros = require('cors')
+
 const port = 3000
+
+exp.use(cros())
 
 http.listen(port, () => console.log(`server listening on port: ${port}`))
 
